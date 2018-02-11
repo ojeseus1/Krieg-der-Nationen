4a1a1a5ed629dc737da9405e690a549eba91fcb3
ojeseus1 <ojeseus1@jproggy.org>
2018-02-11 15:31:30 +0100

    Spieler Bewegung

---
+++ Assets/Startszener/Spieler_bewegung.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spieler_bewegung : MonoBehaviour {
+
+    public float speed = 5f;
+    Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movment = new Vector3(moveHorizontal, 0f, moveVertical);
+        myRigidbody.AddForce(movment * speed);
+	}
+}
