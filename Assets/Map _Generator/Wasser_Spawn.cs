f2990d392d43f5eed7cb5dc19426b9b331029da0
ojeseus1 <ojeseus1@jproggy.org>
2018-02-14 20:49:00 +0100

    zufällig Wasser_Spawn(noch nicht fertig)

---
+++ Assets/Map _Generator/Wasser_Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wasser_Spawn : MonoBehaviour {
+    public Transform spawn_posision;
+    public GameObject prefabe1;
+    public int anzahl;
+    private Vector3 posrand;
+	
+	void Start () {
+        Invoke("spawnone", 2);
+	}
+    void spawnone()
+    {
+        for (int i = 0; i < anzahl; i++)
+        {
+            posrand = Random.insideUnitSphere * 5;
+            Instantiate(prefabe1, spawn_posision.transform.position + posrand, prefabe1.transform.rotation);
+        }
+    }
+}
