91387b10468cfed658ef7a89fc0f9404645aa5d7
ojeseus1 <ojeseus1@jproggy.org>
2018-02-15 20:25:10 +0100

    Ingame_Menü mit Esc öffnen

---
+++ Assets/ingamemenü/Esc_Ingamemenü.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Esc_Ingamemenü : MonoBehaviour {
+
+    private bool IsPause = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Ingamemenü", LoadSceneMode.Additive);
+            ToggleTimeScale();
+        }
+
+	}
+
+    void ToggleTimeScale()
+    {
+        if(!IsPause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        IsPause = !IsPause;
+    }
+}
