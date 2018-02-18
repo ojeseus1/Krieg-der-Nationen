d83766191ce58acb7ef7cb197a67d15336731520
ojeseus1 <ojeseus1@jproggy.org>
2018-02-18 20:24:59 +0100

    nicht fertig

--- Assets/ingamemenü/Spiel_verlassen.cs
+++ Assets/ingamemenü/Spiel_verlassen.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spiel_verlassen : MonoBehaviour {
 
-	
+    
 	// Update is called once per frame
 	void Update () {
-		
+	    
 	}
+    void Ingamemenü()
+    {
+        SceneManager.LoadScene("Startseite", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Startscene");
+        SceneManager.UnloadSceneAsync("Ingamemenü");
+    }
 }
