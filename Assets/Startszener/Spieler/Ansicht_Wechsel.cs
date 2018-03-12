31a56385206d786b8d94a294db4d33d3f464fa65
ojeseus1 <ojeseus1@jproggy.org>
2018-03-12 14:20:50 +0100

    R_Kartenmodus noch nicht fertig

---
+++ Assets/Startszener/Spieler/Ansicht_Wechsel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ansicht_Wechsel : MonoBehaviour {
+
+    public Camera kamera1;
+    public Camera kamera2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            kamera1.enabled = !kamera1.enabled;
+            kamera2.enabled = !kamera2.enabled;
+        }
+    }
+}
